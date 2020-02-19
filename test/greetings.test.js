d53bad59de7eeaf28be840669f760f29ebfaa34b
zeke <emostov@middlebury.edu>
2020-02-19 00:17:52 -0800

    setup beforeEach in test

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const assert = require('assert');
 const ganache = require('ganache-cli'); // local ethe network
 const Web3 = require('web3'); // constructor function
@@ -6,3 +7,18 @@ const Web3 = require('web3'); // constructor function
 const web3 = new Web3(ganache.provider());
 
 
+beforeEach(() => {
+  // ganache auto creates accounts in testing
+  // .eth is the object specifically for ethereum block chain interactions
+  // getAccounts retrieves all accounts and is async - returning a promise
+  web3.eth.getAccounts()
+    .then((fetchedAccounts) => {
+      console.log(fetchedAccounts);
+    });
+});
+
+describe('Greetings Contract', () => {
+  it('test stub', () => {
+
+  });
+});

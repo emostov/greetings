0ab90980fee7441a359a9b48100c7b84be3cbe7b
zeke <emostov@middlebury.edu>
2020-02-18 23:23:13 -0800

    resolve solc version discrepancies

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+
+# See all features with global installed solc command line tool
+```
+solcjs --help
+```
+Currently using version 0.4.22
--- compile.js
+++ compile.js
@@ -1,6 +1,14 @@
-import path from 'path';
-import fs from 'fs';
-import solc from 'solc';
+/* eslint-disable no-console */
+
+// Why is es6 syntax not work?
+// import * as path from 'path';
+// import * as fs from 'fs';
+// import * as solc from 'solc';
+
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs');
+
 
 const greetingsPath = path.resolve(__dirname, 'contracts', 'greetings.sol');
 

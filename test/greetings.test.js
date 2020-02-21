59fec133afed2d1956984b960586524870c850ed
zeke <emostov@middlebury.edu>
2020-02-20 20:55:13 -0800

    save before closing

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -11,11 +11,12 @@ const { interface, bytecode } = require('../compile');
 const Web3 = new web3(ganache.provider());
 const MILlION_WIE = '1000000'
 let greetings;
+let accounts
 
 beforeEach((done) => {
 
   //bytecode is contract, and args is args for contract constructor
-  const deployTransactionObj = { data: bytecode, arguments: ['Hello World'] }
+  const deployTransactionObj = { data: bytecode, arguments: ['Mostov Contract'] }
 
   // 'from:' account creating contract, no 'to:' when deploying
   // can also specify gasPrice, value, nonce
@@ -23,22 +24,36 @@ beforeEach((done) => {
 
   //.eth referes to eth object of web3
   // getAccounts retrieves all acounts in the network
-  Web3.eth.getAccounts().then((accounts) => {
+  Web3.eth.getAccounts().then((accnts) => {
+    accounts = accnts;
     //these are nested so send has access to the first account
     new Web3.eth.Contract(JSON.parse(interface))
       .deploy(deployTransactionObj)
-      .send({ from: accounts[0], gas: MILlION_WIE })
+      .send({ from: accnts[0], gas: MILlION_WIE })
       .then((deployedContract) => {
         greetings = deployedContract;
-        done()
-      })
+        done();
+      });
   });
 });
 
 describe('Greetings', () => {
-  it('it deploys greetings contract to a valid address', () => {
+  it('deploys greetings contract to a valid address', () => {
     console.log(greetings);
     assert.ok(greetings.options.address);
+  });
+
+  it('has a default message of "Mostov Contract"', async () => {
+    const defaultMessage = await greetings.methods.message().call();
+    // call is a read therefore requires no gas
+    assert.equal(defaultMessage, 'Mostov Contract')
+  })
+
+  it('can overwrite message with "spread lavendar"', async () => {
+    await greetings.methods.setMessage('spread lavendar')
+      .send({ from: accounts[0] }) //TODO check where gas is coming from
+    const overwriteMessage = await greetings.methods.message().call();
+    assert.equal(overwriteMessage, 'spread lavendar');
   });
 });
 
